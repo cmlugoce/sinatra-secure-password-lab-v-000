6af85414cd342f6425da385afd0b863f7b38ad36
Cristina Lugo <cmlugoce@syr.edu>
2018-07-15 20:24:13 +0000

    automatically backed up by learn

--- db/migrate/20180715200849_create_users.rb
+++ db/migrate/20180715200849_create_users.rb
@@ -1,12 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
-  def up 
+  def change
     drop_table :users 
   end 
   
-  def down
-    create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-    end
-  end
+ 
 end
